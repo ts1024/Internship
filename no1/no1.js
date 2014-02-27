dfcd8b82c2fc07708402ec2298b6813855978956
ts1024 <git.ts1024@gmail.com>
2014-02-27 12:58:52 +0900

    no1 a & b & c

---
+++ no1/no1.js
@@ -0,0 +1,38 @@
+/********* no1 a) *********/
+function fn_no1_a (str, output){
+	var charList = {};
+	for (var i = 0, len = str.length; i < len; i++) {
+		if (charList[str[i]]) {
+			output("false");
+			return;
+		}
+		charList[str[i]] = true;
+	}
+	output("true");
+}
+
+/********* no1 b) *********/
+function isSubstring(str1, str2){
+	return str1.indexOf(str2) !== -1;
+}
+
+function fn_no1_b (str1, str2, output){
+	if (str1 && str2) {
+		output(isSubstring(str1, str2));
+	} else {
+		output("");
+	}
+}
+
+/********* no1 c) *********/
+function fn_no1_c (str1, str2, output){
+	if (str1 && str2) {
+		if (str1.length === str2.length) {
+			output(isSubstring(str1 + str1, str2));
+		} else {
+			output("false");
+		}
+	} else {
+		output("");
+	}
+}
